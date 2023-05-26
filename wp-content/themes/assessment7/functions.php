94e7524b0c2bd73b4e455ef91665970ca7f88e46
Patrick Mwaniki <patrickmwanikk@gmail.com>
2023-05-26 11:43:01 +0300

    login update

--- wp-content/themes/assessment7/functions.php
+++ wp-content/themes/assessment7/functions.php
@@ -21,75 +21,49 @@ register_nav_menu('primary', 'Primary Header');
 register_nav_menu('secondary', 'Footer Navigation');
 
 //limit login attempts
+add_filter('authenticate', 'check_attempted_login', 30, 3);
+add_action('wp_login_failed', 'login_failed', 10, 1);
+
 function check_attempted_login($user, $username, $password) {
-    if (get_transient('attempted_login')) {
-        $datas = get_transient('attempted_login');
+    $attemptData = get_attempt_data();
 
-        if ($datas['tried'] >= 3) {
-            $until = get_option('_transient_timeout_attempted_login');
-            $time = time_to_go($until);
-
-            return new WP_Error('too_many_tried', sprintf(__('<strong>ERROR</strong>: You have reached the authentication limit, please try again after %1$s'), $time));
-        }
+    if ($attemptData['tried'] >= 5) {
+        $time = calculate_time_remaining($attemptData['start_time'], 180);
+        return new WP_Error('too_many_tried', sprintf(__('<strong>ERROR</strong>: You have reached the authentication limit. Please try again after %1$s.'), $time));
     }
 
     return $user;
 }
 
-add_filter('authenticate', 'check_attempted_login', 30, 3);
-
 function login_failed($username) {
-    if (get_transient('attempted_login')) {
-        $datas = get_transient('attempted_login');
-        $datas['tried']++;
+    $attemptData = get_attempt_data();
 
-        if ($datas['tried'] <= 3) {
-            set_transient('attempted_login', $datas, 300);
-        } else {
-            delete_transient('attempted_login');
-        }
+    if ($attemptData['tried'] >= 5) {
+        $attemptData['start_time'] = time();
+        $attemptData['tried'] = 1;
     } else {
-        $datas = array('tried' => 1);
-        set_transient('attempted_login', $datas, 300);
+        $attemptData['tried']++;
+    }
+
+    update_attempt_data($attemptData);
+}
+
+function calculate_time_remaining($startTime, $expiration) {
+    $remainingTime = $startTime + $expiration - time();
+
+    if ($remainingTime < 60) {
+        return $remainingTime . ' seconds';
+    } else {
+        $minutes = floor($remainingTime / 60);
+        return $minutes . ' minute' . ($minutes > 1 ? 's' : '');
     }
 }
 
-add_action('wp_login_failed', 'login_failed', 10, 1);
+function get_attempt_data() {
+    $attemptData = get_transient('attempted_login');
+    return ($attemptData !== false) ? $attemptData : array('tried' => 0, 'start_time' => 0);
+}
 
-function time_to_go($timestamp) {
-    $periods = array(
-        "second",
-        "minute",
-        "hour",
-        "day",
-        "week",
-        "month",
-        "year"
-    );
-
-    $lengths = array(
-        "60",
-        "60",
-        "24",
-        "7",
-        "4.35",
-        "12"
-    );
-
-    $current_timestamp = time();
-    $difference = abs($current_timestamp - $timestamp);
-
-    for ($i = 0; $difference >= $lengths[$i] && $i < count($lengths) - 1; $i++) {
-        $difference /= $lengths[$i];
-    }
-
-    $difference = round($difference);
-
-    if (isset($difference)) {
-        if ($difference != 1) {
-            $periods[$i] .= "s";
-        }
-        $output = "$difference $periods[$i]";
-        return $output;
-    }
+function update_attempt_data($attemptData) {
+    set_transient('attempted_login', $attemptData, 180);
 }
